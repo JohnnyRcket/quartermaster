7bbbe5cbaaba5f3031356f26ce8604193ef2173e
JohnnyRcket <bizentine@yahoo.com>
2025-04-18 14:57:36 -0700

    fixed live editing and modal switchboard for animal and character modals

--- src/app/modals/animal-modal.component.ts
+++ src/app/modals/animal-modal.component.ts
@@ -10,56 +10,64 @@ import {ANIMALS} from '../main-page.component';
   selector: 'app-animal-modal',
   standalone: true,
   template: `
-         <div class="modal-content">
+      <div class="modal-content">
           <div class="modal-header">
-            <h4 class="modal-title">Animal</h4>
-            <button class="btn-close" type="button" aria-label="Close" (click)="closeModal()"></button>
+              <h4 class="modal-title">{{title}}</h4>
+              <button class="btn-close" type="button" aria-label="Close" (click)="closeModal()"></button>
           </div>
           <div class="modal-body">
-            <div class="row">
-              <div class="col">
-                <span>Name: </span>
-                <input class="form-control-sm mb-0 pb-1" type="text" id="animalName" [(ngModel)]="carrier.name">
+              <div class="row">
+                  <div class="col">
+                      <span>Name: </span>
+                      <input class="form-control-sm mb-0 pb-1" type="text" id="animalName"
+                             [(ngModel)]="bufferCarrier.name">
+                  </div>
+                  <div class="col">
+                      <span>Capacity: </span>
+                      <input class="form-control-sm" type="number" id="animalCapacity"
+                             [(ngModel)]="bufferCarrier.capacity">
+                  </div>
               </div>
-              <div class="col">
-                <span>Capacity: </span>
-                <input class="form-control-sm" type="number" id="animalCapacity" [(ngModel)]="carrier.capacity">
-              </div>
-            </div>
           </div>
           <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" (click)="save()">Save</button>
-            <button class="btn btn-secondary" type="button" (click)="delete()">Delete</button>
-            <button class="btn btn-secondary" type="button" (click)="closeModal()">Close</button>
+              <button class="btn btn-secondary" type="button" (click)="save()">Save</button>
+              <button class="btn btn-secondary" type="button" (click)="delete()">Delete</button>
+              <button class="btn btn-secondary" type="button" (click)="closeModal()">Close</button>
           </div>
-        </div>
+      </div>
   `,
   imports: [
     FormsModule
   ]
 })
 export class AnimalModalComponent {
-  @Input() editCarrier?: Carrier;
-  carrier = new Carrier(uuidv4(), "", 0, [], CarrierType.Animal)
+  @Input() existingCarrier: Carrier | null = null;
+  bufferCarrier = new Carrier(uuidv4(), "", 0, [], CarrierType.Animal)
+  title: string = "New Animal";
 
   constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit() {
-    if (this.editCarrier){this.carrier = this.editCarrier}
+    if (this.existingCarrier){
+      console.log(this.existingCarrier)
+      Object.assign(this.bufferCarrier, this.existingCarrier);
+      this.title = "Edit Animal"
+    }
   }
   closeModal(){
     this.activeModal.close();
   }
 
   save(){
-    if (!this.editCarrier) {ANIMALS.push(this.carrier)}
+    if (this.existingCarrier) {Object.assign(this.existingCarrier, this.bufferCarrier);}
+    else {ANIMALS.push(this.bufferCarrier)}
     console.log(ANIMALS)
     this.closeModal();
   }
 
   delete() {
-    if (this.editCarrier) {
-      const index = ANIMALS.indexOf(this.editCarrier);
+    if (this.existingCarrier) {
+      const index = ANIMALS.indexOf(this.existingCarrier);
       if (index !== -1) {
         ANIMALS.splice(index, 1);
       }
